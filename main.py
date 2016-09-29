2dfea041e941f5a6f10c0aaf623bb9dddeb9d954
Dominic Wang <hughwung@gmail.com>
2016-09-29 09:25:47 -0400

    Implemented: Basic Template, Team Signature, Login Window, Custom 404,

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, g
 app = Flask(__name__)
 app.config['DEBUG'] = True
+
+teamname="Team Awesome"
+wpi_logo='WPI_Inst_Prim_FulClr_Rev.png'
+
+with app.app_context():
+	pass
+
+def run_before_request():
+	g.wpi_logo_url = url_for('static', filename=wpi_logo)
+
+app.before_request(run_before_request)
 
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
@@ -9,10 +20,15 @@ app.config['DEBUG'] = True
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Hello World! <br>--Team Awesome'
+    return render_template('hello.html',
+    						teamname=teamname,
+    						img_url=g.wpi_logo_url,
+    						css_url=url_for('static', filename='login.css'))
 
 
 @app.errorhandler(404)
 def page_not_found(e):
     """Return a custom 404 error."""
-    return 'Sorry, nothing at this URL.', 404
+    return render_template('404.html',
+    						teamname=teamname,
+    						img_url=g.wpi_logo_url), 404
