530fbb0d835f3d6b1fb15fb94a58548c259727a5
Wung Hugh <hughwung@gmail.com>
2016-10-08 01:17:19 +0000

    Fix guest

--- renderers.py
+++ renderers.py
@@ -1,13 +1,13 @@
 from flask                import render_template, url_for, session, request
 from user                 import User
 import logging
-from user                 import User
 import settings
 
 
 def helloLoginRenderer(**kwargs):
     return render_template('helloLogin.html',
                             css_url=url_for('static', filename='login.css'),
+                            guest=User.getUser('guest'),
                             **kwargs)
 def dashboardRenderer():
 
